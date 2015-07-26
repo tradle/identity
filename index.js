e7a72560ca6f5001ed19cdfd29773cacfbadf65c
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-07-26 10:36:55 -0400

    don't export kiki anymore

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-var extend = require('extend')
-var kiki = require('kiki')
 
-// TODO: remove kiki (after migration)
-module.exports = extend({
+module.exports = {
   Sections: require('./lib/sectionTypes'),
   Identity: require('./lib/identity'),
   AddressBook: require('./lib/addressbook'),
   defaultKeySet: require('./lib/defaultKeySet')
-}, kiki)
+}
